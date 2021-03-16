280fccabb15944b6f12696a7ef5506d524e0f733
benvinert <benvinerttt@gmail.com>
2021-03-16 16:00:30 +0200

    add urls

--- vinerfia_Project/vinerfia_Project/urls.py
+++ vinerfia_Project/vinerfia_Project/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.view.static import serve
+from django.conf.urls import url
 
 
 urlpatterns = [
@@ -22,5 +24,8 @@ urlpatterns = [
     path('api/',include('vinerfiav1.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     
 ]
