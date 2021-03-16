74410f64e9a68272675307df14ae7e97fbe25aa4
benvinert <benvinerttt@gmail.com>
2021-03-16 16:10:24 +0200

    Remove static files

--- vinerfia_Project/vinerfia_Project/urls.py
+++ vinerfia_Project/vinerfia_Project/urls.py
@@ -15,17 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.view.static import serve
-from django.conf.urls import url
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('vinerfiav1.urls')),
     path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
-
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path('auth/', include('djoser.urls.jwt'))
     
 ]
