c002bc9a4a8d42d3f824e84e4085835879ea6a0f
benvinert <benvinerttt@gmail.com>
2021-03-17 13:38:11 +0200

    dsadsafasfsa

--- vinerfia_Project/vinerfiav1/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Vinerfiav1Config(AppConfig):
-    name = 'vinerfiav1'
--- vinerfia_Project/vinerfiav1/views/__init__.py
+++
@@ -1,4 +0,0 @@
-from .PostsView import *
-from .ItemsView import *
-from .OrdersView import *
-from .Admin import *
