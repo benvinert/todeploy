d230bb8962e3b4fc48cb947ed1e2bbf38e6fb82a
benvinert <64857968+benvinert@users.noreply.github.com>
2021-03-18 13:00:43 +0200

    Update settings.py

--- vinerfia_Project/settings.py
+++ vinerfia_Project/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'djoser',
     'UserAuth',
     'vinerfiav1.apps.Vinerfiav1Config',
@@ -59,12 +60,22 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://front-ben-ecommerce.herokuapp.com",
+    "http://localhost:8080",
+    "http://127.0.0.1:9000"
 ]
 
 ROOT_URLCONF = 'vinerfia_Project.urls'
