b19c455056d84d7ab82600063016583aaa95de48
benvinert <64857968+benvinert@users.noreply.github.com>
2021-03-23 14:46:25 +0200

    Update Login.js

--- src/Authentication/Login.js
+++ src/Authentication/Login.js
@@ -59,7 +59,7 @@ export default function SignIn() {
   const loadUser = async(user_access) => {
     localStorage.setItem("access",user_access)
     const req = await fetch("https://backend-ben-ecommerce.herokuapp.com/auth/users/me",{
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      method: 'GET', // *GET, POST, PUT, DELETE, etc.
       mode: 'cors', // no-cors, *cors, same-origin
       cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
       credentials: 'same-origin', // include, *same-origin, omit
