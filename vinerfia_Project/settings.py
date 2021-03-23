d69a85e0ca351b6b88bcc3ada3dd8a33a51d848b
benvinert <64857968+benvinert@users.noreply.github.com>
2021-03-23 10:54:38 +0200

    Update settings.py

--- vinerfia_Project/settings.py
+++ vinerfia_Project/settings.py
@@ -114,7 +114,9 @@ DATABASES = {
     }}
 
 import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
