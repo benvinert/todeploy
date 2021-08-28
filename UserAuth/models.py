64e826d6f96c6551ef4d9ac922b035263e7edc96
benvinert <benvinerttt@gmail.com>
2021-08-28 18:30:56 +0300

    staff ON

--- UserAuth/models.py
+++ UserAuth/models.py
@@ -22,7 +22,7 @@ class UserAccount(AbstractBaseUser,PermissionsMixin):
     email = models.EmailField(max_length=255,unique=True)
     name = models.CharField(max_length=200)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=True)
     birthday = models.CharField(max_length=10)
     is_superuser = None
 
